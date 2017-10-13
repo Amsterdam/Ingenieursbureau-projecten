0e0985b59f02ad30340cf5ac8487419aa0a7a94f
e.jager@amsterdam.nl <info@lytrix.com>
2017-10-13 13:08:17 +0200

    changed static path

--- web/ibprojecten/settings.py
+++ web/ibprojecten/settings.py
@@ -198,7 +198,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ibprojecten/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..','static'))
 
 
